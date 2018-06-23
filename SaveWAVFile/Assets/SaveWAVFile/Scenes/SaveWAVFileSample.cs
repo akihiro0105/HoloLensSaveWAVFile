6f54305d17d57bad8b0104f5d4a15a9300168abc
晃弘 上山 <akihiro@hololab.co.jp>
2018-06-24 04:07:00 +0900

    wavファイルの保存，読み込み，ストリーミング保存の関数定義

--- SaveWAVFile/Assets/SaveWAVFile/Scenes/SaveWAVFileSample.cs
+++ SaveWAVFile/Assets/SaveWAVFile/Scenes/SaveWAVFileSample.cs
@@ -1,16 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
+using SaveWAVFile;
 
 public class SaveWAVFileSample : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private bool audioStopFlag = true;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(AudioCoroutine(null));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    private IEnumerator AudioCoroutine(Action close)
+    {
+        int frequency = 44100;
+        float[] databuffer = new float[frequency];
+        float[] processbuffer = new float[256];
+        int head = 0;
+
+        AudioClip audioClip = Microphone.Start(null, true, 1, frequency);
+        if (!(Microphone.GetPosition(null) > 0)) { }
+        while (audioStopFlag)
+        {
+            var tail = Microphone.GetPosition(null);
+            if (tail > 0&&head!= tail)
+            {
+                audioClip.GetData(databuffer, 0);
+                while (true)
+                {
+                    var datalength = DataLength(head, tail, databuffer.Length);
+                    if (datalength > processbuffer.Length)
+                    {
+                        var offset = databuffer.Length - head;
+                        if (offset<processbuffer.Length)
+                        {
+                            Array.Copy(databuffer, head, processbuffer, 0, offset);
+                            Array.Copy(databuffer, 0, processbuffer, offset, processbuffer.Length - offset);
+                        }
+                        else
+                        {
+                            Array.Copy(databuffer, head, processbuffer, 0, processbuffer.Length);
+                        }
+
+                        // processbuffer
+
+                        head += processbuffer.Length;
+                        if (head>databuffer.Length)
+                        {
+                            head -= databuffer.Length;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            yield return null;
+        }
+        Microphone.End(null);
+        if (close != null) close.Invoke();
+    }
+
+    private int DataLength(int head,int tail,int length)
+    {
+        if (head<tail)
+        {
+            return tail - head;
+        }
+        else
+        {
+            return length - (tail - head);
+        }
+    }
+
+    void OnDestroy()
+    {
+        audioStopFlag = false;
+    }
 }
---
+++ SaveWAVFile/Assets/SaveWAVFile/Scripts/WAVFileControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveWAVFile
+{
+    public class WAVFileControl
+    {
+        private const int frequency = 44100;
+        // Simple Recoder
+        public static IEnumerator StartRecordData(string name,int second, Action<AudioClip> complete)
+        {
+            var audioClip = Microphone.Start(null, false, second, frequency);
+            yield return new WaitWhile(() => Microphone.IsRecording(null));
+            yield return CreateWAVData(audioClip, name, null);
+            if (complete != null) complete.Invoke(audioClip);
+        }
+
+        public static void StopRecordData()
+        {
+            Microphone.End(null);
+        }
+
+        // WAV IO
+        public static IEnumerator CreateWAVData(AudioClip audioClip, string name, Action complete)
+        {
+            // Save local folder
+            yield return null;
+        }
+        public static IEnumerator LoadWAVData(string name, Action<AudioClip> action)
+        {
+            // Load local folder
+            string localpath = "\\";
+            var path = "file://" + localpath + name;
+            using (WWW www = new WWW(path))
+            {
+                yield return www;
+                var audioclip = www.GetAudioClip(true, true);
+                if (action != null) action.Invoke(audioclip);
+            }
+        }
+
+        // Stream Recorder
+
+    }
+}
